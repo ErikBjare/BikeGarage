49b7b342114f261b781180e6514d313acd1f5042
Filip Harald <filip.harald@gmail.com>
2014-05-13 08:58:43 +0200

    försöker ladda upp

--- src/BikeOwner.java
+++ src/BikeOwner.java
@@ -1,9 +1,32 @@
+import java.util.ArrayList;
+
 
 public class BikeOwner {
 	String name;
 	String ssn;
 	String email;
-	Bike bikes[];
-	String in;
+	ArrayList<Bike> bikes;
+	String pin;
+	
+	public BikeOwner(String name, String ssn, String email){
+		this.name = name;
+		this.ssn = ssn;
+		this.email = email;
+		bikes = new ArrayList<Bike>();
+		//ToDo: skicka med boms i konstruktorn?
+		//pin = boms.generatePIN();
+		
+	}
+	
+	public void addBike(Bike bike){
+		bikes.add(bike);
+	}
+	
+	public void removeBike(Bike bike){
+		bikes.remove(bike);
+	}
+	public void removeAllBikes(){
+		bikes = null;
+	}
 	
 }
